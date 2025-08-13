93feb36a611fbb00c8025a8d37a0ce44fa8d9815
unknowenuser <61786848+unknowenuser@users.noreply.github.com>
2025-08-13 22:54:22 +0900

    ver2

--- leetcode/code_writing_space/a.cpp
+++ leetcode/code_writing_space/a.cpp
@@ -6,12 +6,9 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i = m - 1,j = n - 1,k = m + n - 1; //i nums1 j nums2 k nums1+nums2
-        for (; k >= 0; k--)
+        while (j >= 0)
         {
-            if (j < 0)
-            {
-                break;
-            }
+            
             if (i >= 0 && nums1[i] > nums2[j])
             {
                 nums1[k] = nums1[i];

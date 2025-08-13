44efc9775719609e5b6e328993e6e3ff13f445c0
unknowenuser <61786848+unknowenuser@users.noreply.github.com>
2025-08-14 01:05:09 +0900

    ver 1

--- leetcode/code_writing_space/a.cpp
+++ leetcode/code_writing_space/a.cpp
@@ -6,22 +6,31 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        bool twice = false;
         size_t index = 0;
         for (size_t i = 1; i < nums.size(); i++)
         {
-            if (nums[index] != nums[i])
+            if (twice == false && nums[index] == nums[i])
+            {
+                twice = true;
+                nums[index++] = nums[i];
+                nums[index] = nums[i];
+                continue;
+            }
+            if (nums[i] != nums[index])
             {
                 nums[++index] = nums[i];
+                twice = false;
             }
         }
-        return index + 1;
+        return (int)(index + 1);
     }
 };
 
 int main()
 {
     Solution a;
-    vector<int> n1 = {0,0,1,2,3,3,4,5,6,6,7,7,7,8,9,10,10};
+    vector<int> n1 = {0,0,0,1,2,3,3,3,4,5,6,6,7,7,7,8,9,10,10};
     int k = a.removeDuplicates(n1);
     cout << "k: " << k << "\n" << "nums:";
     for (auto i : n1)

366c5975e9325b081b332966ca29977b0bb4db3d
unknowenuser <61786848+unknowenuser@users.noreply.github.com>
2025-08-14 00:25:15 +0900

    ver1(easy to code,too much waste)

--- leetcode/code_writing_space/a.cpp
+++ leetcode/code_writing_space/a.cpp
@@ -4,34 +4,28 @@ using namespace std;
 
 
 class Solution {
-
 public:
-
-    int removeElement(vector<int>& nums, int val) {
-
-        int k = 0; 
-
-        for (int i = 0; i < nums.size(); i++) {
-
-            if (nums[i] != val) {
-
-                nums[k++] = nums[i];
-
+    int removeDuplicates(vector<int>& nums) {
+        vector<int> result = {nums[0]};
+        result.reserve(nums.size());
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+            if (result[result.size()-1] != nums[i])
+            {
+                result.push_back(nums[i]);
             }
-
         }
-
-        return k; 
-
+        result.shrink_to_fit();
+        nums = move(result);
+        return (int)nums.size();
     }
-
 };
 
 int main()
 {
     Solution a;
-    vector<int> n1 = {};
-    int k = a.removeElement(n1,0);
+    vector<int> n1 = {0,0,1,2,3,3,4,5,6,6,7,7,7,8,9,10,10};
+    int k = a.removeDuplicates(n1);
     cout << "k: " << k << "\n" << "nums:";
     for (auto i : n1)
     {

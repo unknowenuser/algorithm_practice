ee2bdfe298a135cf2a0d91afed6267e214bf35c8
unknowenuser <61786848+unknowenuser@users.noreply.github.com>
2025-08-13 23:59:23 +0900

    hotfix

--- leetcode/code_writing_space/a.cpp
+++ leetcode/code_writing_space/a.cpp
@@ -6,14 +6,16 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int k = (int)nums.size();
-        int index = k - 1,i = 9;
+        int index = k - 1,i = 0;
         while (i <= index)
         {
             if (nums[(size_t)i] == val)
             {
-                swap(nums[(size_t)i--],nums[(size_t)index--]);
+                swap(nums[(size_t)i],nums[(size_t)index--]);
                 k--;
+                continue;
             }
+            i++;
         }
         return k;
     }

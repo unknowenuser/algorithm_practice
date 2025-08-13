aadadfadc2fda8b4d1bcb16feb60a844e55b356b
unknowenuser <61786848+unknowenuser@users.noreply.github.com>
2025-08-14 00:34:59 +0900

    ver2

--- leetcode/code_writing_space/a.cpp
+++ leetcode/code_writing_space/a.cpp
@@ -6,18 +6,15 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        vector<int> result = {nums[0]};
-        result.reserve(nums.size());
-        for (size_t i = 0; i < nums.size(); i++)
+        size_t index = 0;
+        for (size_t i = 1; i < nums.size(); i++)
         {
-            if (result[result.size()-1] != nums[i])
+            if (nums[index] != nums[i])
             {
-                result.push_back(nums[i]);
+                nums[++index] = nums[i];
             }
         }
-        result.shrink_to_fit();
-        nums = move(result);
-        return (int)nums.size();
+        return index + 1;
     }
 };
 

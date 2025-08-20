2eaf277b500c2594096d7c9e644941811f20a2fe
unknowenuser <61786848+unknowenuser@users.noreply.github.com>
2025-08-20 18:25:24 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ leetcode/0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool canJump(std::vector<int>& nums) {
+        int max_index = static_cast<int>(nums.size() - 1);
+        if (max_index == 0)
+        {
+            return true;
+        }
+        if (nums[0] == 0)
+        {
+            return false;
+        }
+        
+        if (nums[max_index] == 0)
+        {
+            int i = max_index - 1;
+            while (i >= 0)
+            {
+                if (max_index - i <= nums[i])
+                {
+                    max_index = i;
+                    break;
+                }
+                --i;
+            }
+            if (i < 0)
+            {
+                return false;
+            }
+            
+        }
+        for (int i = max_index; i >= 0; i--)
+        {
+            if (nums[i] < 1) // == (nums[i] == 0)
+            {
+                int j = i - 1;
+                for (; j >= 0; j--)
+                {
+                    if (i - j < nums[j])
+                    {
+                        i = j - 1;
+                        break;
+                    }
+                }
+                if (j < 0)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};

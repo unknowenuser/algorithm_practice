1583b672cb961e70c1b7d24a64aba5305774d0de
unknowenuser <61786848+unknowenuser@users.noreply.github.com>
2025-08-20 17:10:28 +0900

    faster ver

--- leetcode/code_writing_space/a.cpp
+++ leetcode/code_writing_space/a.cpp
@@ -8,7 +8,6 @@ public:
     int maxProfit(std::vector<int>& prices) {
         int buy = prices[0];
         int profit = 0;
-        
         for (size_t i = 1; i < prices.size(); i++) {
             if (prices[i] > buy) {
                 profit += (prices[i] - buy);

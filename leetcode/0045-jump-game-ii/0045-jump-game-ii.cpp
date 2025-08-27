1fb15874c0f016e6734dea59e6a41ebcfc21a1db
unknowenuser <61786848+unknowenuser@users.noreply.github.com>
2025-08-27 20:13:20 +0900

    Time: 0 ms (100%), Space: 20.4 MB (63.24%) - LeetHub

--- leetcode/0045-jump-game-ii/0045-jump-game-ii.cpp
+++ leetcode/0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    int jump(std::vector<int>& nums) {
-        if (nums.size() == 1)
-        {
-            return 0;
-        }
-        int counter = 1;
-        for (size_t i = 0; nums.size() > nums[i] + i;)
-        {
-            size_t max = i;
-            for (size_t j = i + 1; j <= nums[i] + i; ++j)
-            {
-                if (nums[j] + j > nums[max] + max)
-                {
-                    max = nums[j] + j;
-                }
+    int jump(vector<int>& nums) {
+        int jumps=0,end=0,farthest=0;
+        for(int i=0;i<nums.size()-1;i++) {
+            farthest=max(farthest,i+nums[i]);
+            if(i==end) {
+                jumps++;
+                end=farthest;
             }
-            i = max;
-            ++counter;
         }
-        return counter;
+        return jumps;
     }
 };

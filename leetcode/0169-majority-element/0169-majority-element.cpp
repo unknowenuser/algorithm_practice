e7098d5a1e48c98a4a08d55388afe3ba36fc8e70
unknowenuser <61786848+unknowenuser@users.noreply.github.com>
2025-08-27 20:15:14 +0900

    Time: 7 ms (11.42%), Space: 28.2 MB (62.96%) - LeetHub

--- leetcode/0169-majority-element/0169-majority-element.cpp
+++ leetcode/0169-majority-element/0169-majority-element.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        size_t n = nums.size();
-        if (n < 3)
-        {
-            return nums[0];
-        }
         sort(nums.begin(),nums.end());
-        return nums[((nums.size()/2) + 1)];
+        return nums[(nums.size()/2)];
     }
 };

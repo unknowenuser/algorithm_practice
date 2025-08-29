d51adfda0487772a156bf47b96798711200c8f21
unknowenuser <61786848+unknowenuser@users.noreply.github.com>
2025-08-29 17:16:51 +0900

    9012 cpp 완성

---
+++ baekjun/only code/9012/9012.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    size_t n;
+    std::string ps;
+    std::cin >> n;
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cin >> ps;
+        int temp = 0;
+        for (auto &j : ps)
+        {
+            switch (j)
+            {
+            case '(':
+                --temp;
+                break;
+            default:
+                ++temp;
+                break;
+            }
+        }
+        switch (temp)
+        {
+        case 0:
+            std::cout << "YES";
+            break;
+        
+        default:
+            std::cout << "NO";
+            break;
+        }
+        
+    }
+    
+    return 0;
+}

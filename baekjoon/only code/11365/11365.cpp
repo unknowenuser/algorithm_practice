e6d37d9f43b63c0e7e645da6fe0aa3bcd640e229
leehojin <a40966361@gmail.com>
2025-10-18 02:45:29 +0900

    11365.cpp complete

---
+++ baekjoon/only code/11365/11365.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+#include <cmath>
+#include <string>
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    char string[501];
+    std::cin.getline(string, 501);
+    while (strcmp(string, "END") != 0)
+    {
+        size_t index = std::strlen(string);
+        for (size_t i = index; i > 0; i--)
+        {
+            std::cout << string[i - 1];
+        }
+        std::cout << '\n';
+        std::cin.getline(string, 501);
+    }
+    
+    return 0;
+}

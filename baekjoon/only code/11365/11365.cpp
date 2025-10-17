c502e0b8ce9109c0435121ae8dc792682ede8b90
leehojin <a40966361@gmail.com>
2025-10-18 03:06:48 +0900

    optimized

--- baekjoon/only code/11365/11365.cpp
+++ baekjoon/only code/11365/11365.cpp
@@ -3,19 +3,14 @@
 
 int main()
 {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cout.tie(0);
     char string[501];
     std::cin.getline(string, 501);
     while (strcmp(string, "END") != 0)
     {
-        size_t index = std::strlen(string);
-        for (size_t i = index; i > 0; i--)
-        {
-            std::cout << string[i - 1];
-        }
-        std::cout << '\n';
+        char *i = &string[std::strlen(string)];
+        for (; i != (&string[0]); i--) std::putchar(*i);
+        putchar(*i);
+        putchar('\n');
         std::cin.getline(string, 501);
     }
     
